ad132803a1a96562cd52566fbbb18f7f95433c76
Brian <97397197+SamQxd@users.noreply.github.com>
2023-04-12 11:33:49 +0200

    Add files via upload

---
+++ sidepanel.js
@@ -0,0 +1,11 @@
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "600px";
+    document.getElementById("sidebutton").style.marginRight = "10px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("sidebutton").style.marginRight= "0";
+  }
